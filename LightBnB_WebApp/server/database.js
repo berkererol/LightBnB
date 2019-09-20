2c7f77a7c276e513ff681f4775bd7119701e8cc7
berkererol <berkererol@gmail.com>
2019-09-20 10:55:39 +0000

    Implemented addProperty function.

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -21,7 +21,6 @@ const users = require('./json/users.json');
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = (email) => {
-  console.log(email);
   return pool.query(`
     SELECT * FROM users
     WHERE email = $1;
@@ -123,7 +122,7 @@ exports.getAllReservations = getAllReservations;
  */
 
 const getAllProperties = (options, limit = 10) => {
-  
+
   let queryString = `
     SELECT properties.*, AVG(rating) AS average_rating
     FROM properties
@@ -176,13 +175,53 @@ exports.getAllProperties = getAllProperties;
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+const addProperty = function(property) {
+  const queryString = `
+    INSERT INTO properties (
+      owner_id, 
+      title, 
+      description, 
+      thumbnail_photo_url, 
+      cover_photo_url, 
+      cost_per_night, 
+      street, 
+      city, 
+      province, 
+      post_code, 
+      country, 
+      parking_spaces, 
+      number_of_bathrooms, 
+      number_of_bedrooms)
+    VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *;
+  `;
+
+  const queryParams = [
+    property.owner_id, 
+    property.title, 
+    property.description, 
+    property.thumbnail_photo_url, 
+    property.cover_photo_url, 
+    property.cost_per_night, 
+    property.street, 
+    property.city, 
+    property.province, 
+    property.post_code, 
+    property.country, 
+    property.parking_spaces, 
+    property.number_of_bathrooms, 
+    property.number_of_bedrooms
+  ];
+
+  return pool.query(queryString, queryParams)
+  .then(res => res.rows);
 }
+
 exports.addProperty = addProperty;
 
-
-
+// const addProperty = function (property) {
+//   const propertyId = Object.keys(properties).length + 1;
+//   property.id = propertyId;
+//   properties[propertyId] = property;
+//   return Promise.resolve(property);
+// }
